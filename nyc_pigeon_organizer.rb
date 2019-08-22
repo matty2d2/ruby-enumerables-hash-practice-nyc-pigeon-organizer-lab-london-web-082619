9e71ff7f8eac16ac1e3a311fbe5c587474269a01
Matthew Dias Prada <matthew.dias.prada@gmail.com>
2019-08-22 12:44:25 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,20 +1,20 @@
 def nyc_pigeon_organizer(data)
-  final = {}
+  hash = {}
 
   data.each do |first_level, all_other| 
     all_other.each do |category, array|
       array.each do |name|
-        final[name] = {:color => [], :gender => [], :lives => []} 
+        hash[name] = {:color => [], :gender => [], :lives => []} 
       end 
     end 
   end 
   
-  name_keys = final.keys
+  name_keys = hash.keys
   data[:color].each do |bird_color, name|
     name.each do |bird_name|
       name_keys.each do |item|
         if bird_name === item
-          final[item][:color] << bird_color.to_s
+          hash[item][:color] << bird_color.to_s
         end 
       end 
     end 
@@ -24,7 +24,7 @@ def nyc_pigeon_organizer(data)
     name.each do |bird_name|
       name_keys.each do |item|
         if bird_name === item
-          final[item][:gender] << gender.to_s
+          hash[item][:gender] << gender.to_s
         end 
       end 
     end 
@@ -34,11 +34,11 @@ def nyc_pigeon_organizer(data)
     name.each do |bird_name|
       name_keys.each do |item|
         if bird_name === item
-          final[item][:lives] << location
+          hash[item][:lives] << location
         end 
       end 
     end 
   end 
   
-  return final 
+  return hash 
 end
